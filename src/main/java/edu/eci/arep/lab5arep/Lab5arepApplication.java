fe7ad68031d1d728db068179cea5ce68fbd2928a
Your Name <andres.rchaparro@mail.escuelaing.edu.co>
2025-02-28 11:58:17 -0500

    update

---
+++ src/main/java/edu/eci/arep/lab5arep/Lab5arepApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.lab5arep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5arepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5arepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arep/lab5arep/Lab5arepApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.lab5arep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5arepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
